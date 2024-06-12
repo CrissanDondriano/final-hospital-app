2b1812d5f78b6798fb6a8877dde369721a701988
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-12 17:50:42 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,3 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import store from '../store';
+export default router;
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import Vuex from 'vuex';
+import axios from 'axios';
+
+export const store = new Vuex.Store({
+
+
+});
